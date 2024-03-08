77d631f705e05184d8da415e2eeee228361f9499
Raymundo Ramos <ramosraymundo551@gmail.com>
2024-03-08 13:30:01 -0600

    finish_helloWorld

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -1,12 +1,43 @@
 import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'unknow',
+    name: 'Desconocido',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'Pruebita',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
 
 export function App () {
-    return (
-        <div className='section-card'>
-        < TwitterFollowCard userName="kikobeats" name="Enrique Prueba" />
-        < TwitterFollowCard userName="elonmusk" name="Elon Musk" />
-        < TwitterFollowCard userName="casey" name="Casey Prueba" />
-        </div>
-    )
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
 }
